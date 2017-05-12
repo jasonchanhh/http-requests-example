fba91c65dec691f6bea80b8371cb568ff259fc3a
Jason Chan <jc15502@my.bristol.ac.uk>
2017-05-12 20:14:39 +0100

    Added GET requests

--- http-requests-example/http-requests-example/ViewController.swift
+++ http-requests-example/http-requests-example/ViewController.swift
@@ -9,14 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //simple GET request
-        let config = URLSessionConfiguration.default
-        let session = URLSession(configuration: config)
-        let urlString = URL(string: "https://jsonplaceholder.typicode.com/users/2")
+
+    // simplest GET request using shared URLSession
+    func getRequestSimple() {
+        print("GET request example 1")
+        let session = URLSession.shared
+        let urlString = URL(string: "https://reqres.in/api/users/1")
         if let url = urlString {
             let task = session.dataTask(with: url) {
                 (data, response, error) in
@@ -24,13 +22,54 @@ class ViewController: UIViewController {
                     print(error!.localizedDescription)
                 }
                 else {
-                    if let dataString = data {
+                    if let dataString = String(data: data!, encoding: String.Encoding.utf8) {
                         print(dataString)
                     }
                 }
             }
             task.resume()
-        }    }
+        }
+    }
+    
+    
+    // GET request using default URLSessionConfiguration
+    func getRequestDefaultConfig() {
+        print("GET request example 2")
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let urlString = URL(string: "https://reqres.in/api/users?page=2")
+        if let url = urlString {
+            let task = session.dataTask(with: url) {
+                (data, response, error) in
+                if error != nil {
+                    print(error!.localizedDescription)
+                }
+                else {
+                    if let dataString = String(data: data!, encoding: String.Encoding.utf8) {
+                        print(dataString)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func getRequestWithParams() {
+        print("GET request example 3")
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let urlString = URL(string: "https://reqres/api/unknown")
+        if let url = urlString {
+            let request = URLRequest(url: url)
+            request.httpMethod = "GET "
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getRequestSimple()
+        getRequestDefaultConfig()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
